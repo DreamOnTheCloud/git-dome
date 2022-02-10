3cef6fa143c90247b3b8c93a994a379bf8d41a9d
Happy-zhang-yu <848767518@qq.com>
2022-02-10 12:17:05 +0800

    Hot-fix commit1

--- GitTest/src/test.java
+++ GitTest/src/test.java
@@ -2,5 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.printf("HelloGit!");
         System.out.printf("HelloGit!");
+        System.out.printf("Hot-fix修改");
+        System.out.printf("Hot-fix修改");
     }
 }
